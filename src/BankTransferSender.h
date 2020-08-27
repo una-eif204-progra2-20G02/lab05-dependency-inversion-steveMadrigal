76d04d88375c2dda9675247504e87cd36cde2d5e
steveMadrigal <68918625+steveMadrigal@users.noreply.github.com>
2020-08-27 12:16:20 -0600

    Definitivo v1.00

--- src/BankTransferSender.h
+++ src/BankTransferSender.h
@@ -2,16 +2,18 @@
 // Created by Maikol Guzman on 8/24/20.
 //
 
-#ifndef LAB05_DEPENDENCY_INVERSION_SOLUTION_BANKTRANSFERSENDER_H
-#define LAB05_DEPENDENCY_INVERSION_SOLUTION_BANKTRANSFERSENDER_H
-
-
-#include <string>
-
-class BankTransferSender  {
-public:
-    std::string sendPayment() const;
+#ifndef LAB05_DEPENDENCY_INVERSION_SOLUTION_BANKTRANSFERSENDER_H//Conditional of defining Class
+#define LAB05_DEPENDENCY_INVERSION_SOLUTION_BANKTRANSFERSENDER_H//Defining Class
+#include "ISend.h"//Calling ISend Claas .h
+class BankTransferSender: public ISend  {//Inheritance Classes, and declaration class
+public://public data
+    //Constructor with out parameters
+    BankTransferSender();
+    //Destructor
+    ~BankTransferSender();
+    //String Method
+    virtual std::string sendPayment() const override;//Virtual override method
 };
 
 
-#endif //LAB05_DEPENDENCY_INVERSION_SOLUTION_BANKTRANSFERSENDER_H
+#endif //LAB05_DEPENDENCY_INVERSION_SOLUTION_BANKTRANSFERSENDER_H//End if defining
